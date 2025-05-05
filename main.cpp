f548654076d5f62a6f9a2dd17d4feb4140ce6e00
Ken Chang - CS Professor <154487739+adjprof2@users.noreply.github.com>
2025-05-04 17:52:59 -0700

    so;ution

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include "helper_functions.h"
 
+using namespace std;
+
 // ===============
 // implementations before porting to student.hpp
 void ascendingSortArray(int a[], int nSize)
 {
+      for (int i = 0; i < nSize - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < nSize; j++) {
+                  if (a[j] < a[minIndex]) {
+                        minIndex = j;
+                  }
+            }
+            swap (a[i], a[minIndex]);
+
+            cout << "After iteration " << i + 1 << ": ";
+            for (int k = 0; k < nSize; k++) {
+                  cout << a[k] << " ";
+            }
+            cout << endl;
+      }
       // TODO: implement sorting algorithm per requirements from assignment
 }
 
@@ -12,11 +29,39 @@ void ascendingSortArray(int a[], int nSize)
 // implementations before porting to student.hpp
 void descendingSortArray(int a[], int nSize)
 {
+      for (int i = 0; i < nSize - 1; i++) {
+            for (int j = 0; j < nSize - i - 1; j++) {
+                  if (a[j] < a[j + 1]) {
+                        swap (a[j], a[j + 1]);
+                  }
+            }
+            cout << "After iteration " << i + 1 << ": ";
+            for (int k = 0; k < nSize; k++) {
+                  cout << a[k] << " ";
+            }
+            cout << endl;
+
+      }
+      
       // TODO: implement sorting algorithm per requirements from assignment
 }
 
 
 int main() {
+      int arrAsc[]= {65,25,13,22,11};
+      int nSizeAsc = sizeof(arrAsc) / sizeof(arrAsc[0]);
+      cout << "Ascending Sort: " << endl;
+      ascendingSortArray (arrAsc, nSizeAsc);
+      ascendingSortArray (arrAsc, nSizeAsc);
+
+      cout << endl;
+
+      int arrDesc[] = {11,22,13,25,65};
+      int nSizeDesc = sizeof(arrDesc) / sizeof(arrDesc[0]);
+      cout << "Descending Sort: " << endl;
+      descendingSortArray (arrDesc, nSizeDesc);
+
+
     // test your ascendingSortArray and decendingSortArray here before porting to student.hpp
     
     return 0;
--- student.hpp
+++ student.hpp
@@ -3,6 +3,8 @@
 #include <iostream> // print library
 #include "helper_functions.h" // helper functions
 
+using namespace std;
+
 /* derive from helper_functions.cpp
 #include <string> // string library
 using namespace std; // use the standard namespace
@@ -14,8 +16,23 @@ void ascendingSortArray(int a[], int nSize);
 // ===============
 // implementations
 
-void ascendingSortArray(int a[], int n) {
-        // TODO: implement sorting algorithm per requirements from assignment
+void ascendingSortArray(int a[], int nSize) 
+{      for (int i = 0; i < nSize - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < nSize; j++) {
+                  if (a[j] < a[minIndex]) {
+                        minIndex = j;
+                  }
+            }
+            swap (a[i], a[minIndex]);
+
+            cout << "After iteration " << i + 1 << ": ";
+            for (int k = 0; k < nSize; k++) {
+                  cout << a[k] << " ";
+            }
+            cout << endl;
+      }
+              // TODO: implement sorting algorithm per requirements from assignment
 }
 
 // Exercise 2: implement descending bubble sort algorithm for integer array
@@ -25,6 +42,19 @@ void descendingSortArray(int a[], int nSize);
 // implementations
 
 void descendingSortArray(int a[], int nSize)
-{
+{      for (int i = 0; i < nSize - 1; i++) {
+            for (int j = 0; j < nSize - i - 1; j++) {
+                  if (a[j] < a[j + 1]) {
+                        swap (a[j], a[j + 1]);
+                  }
+            }
+            cout << "After iteration " << i + 1 << ": ";
+            for (int k = 0; k < nSize; k++) {
+                  cout << a[k] << " ";
+            }
+            cout << endl;
+
+      }
+      
       // TODO: implement sorting algorithm per requirements from assignment
 }
